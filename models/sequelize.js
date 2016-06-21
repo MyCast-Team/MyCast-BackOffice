73b3b9985609e06e1b45caa7e62d737a158f27d6
Pierre <oxeyo@hotmail.fr>
2016-06-21 14:55:17 +0200

    test connection 4

--- models/sequelize.js
+++ models/sequelize.js
@@ -1,6 +1,15 @@
-var sequelize=require("sequelize");
+var Sequelize=require("sequelize");
+var config    = require('config');  // we use node-config to handle environments
 
-module.exports= new sequelize("mycast","root","",{
+var dbConfig = config.get('development');
+/*module.exports= new sequelize("mycast","root","",{
 	pool:false,
 	host:"localhost"
-});
+});*/
+module.exports = new Sequelize(
+    dbConfig.database,
+    dbConfig.username,
+    dbConfig.password,{
+      host: dbConfig.host,
+      dialect: dbConfig.driver
+}
