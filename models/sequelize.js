1dc7d15be16abd8b28a05efbd1943df06620d4fe
Pierre <oxeyo@hotmail.fr>
2016-06-23 14:38:13 +0200

    test connection 6

--- models/sequelize.js
+++ models/sequelize.js
@@ -5,9 +5,10 @@ require('../env.js');
 
 
 if(process.env.NODE_ENV=="test"){
-
+	console.log(config.get("test").get("host"));
 	var dbConfig =	config.get("test");
 } else{
+		console.log(process.env.NODE_ENV);
 var dbConfig =	config.get('development');
 }
 /*module.exports= new sequelize("mycast","root","",{
